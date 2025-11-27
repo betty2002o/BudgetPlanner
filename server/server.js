7af62ea57372215434126be8e076c9a74d71d039
Betty Chiang <betty2002o@gmail.com>
2025-11-27 00:33:16 -0800

    fixed routing

--- server/server.js
+++ server/server.js
@@ -158,7 +158,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   // React SPA catch-all
-  app.get("*", (req, res) => {
+  app.get(/^\/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
